5b29b7c8161de264185d52b37e1be1d114b49efe
Raj gupta <rajgupta.02.0854@gmail.com>
2026-07-03 22:35:02 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,145 @@
+const products = [
+    {
+        id: 1,
+        name: "E-Tech Ultra X",
+        category: "Smartphones",
+        price: 89999,
+        image:
+            "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+
+    },
+    {
+        id: 2,
+        name: "E-Tech Pro Max",
+        category: "Smartphones",
+        price: 74999,
+        image:
+            "https://images.unsplash.com/photo-1598327105666-5b89351aff97"
+    },
+
+    {
+        id: 3,
+        name: "E-Tech AirBook",
+        category: "Laptops",
+        price: 119999,
+        image:
+            "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+    },
+
+    {
+        id: 4,
+        name: "E-Tech Creator Pro",
+        category: "Laptops",
+        price: 149999,
+        image:
+            "https://images.unsplash.com/photo-1517336714739-489689fd1ca8"
+    },
+
+    {
+        id: 5,
+        name: "E-Tech Studio Headphones",
+        category: "Audio",
+        price: 18999,
+        image:
+            "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+    },
+
+    {
+        id: 6,
+        name: "E-Tech Noise Pro",
+        category: "Audio",
+        price: 12999,
+        image:
+            "https://images.unsplash.com/photo-1484704849700-f032a568e944"
+    },
+
+    {
+        id: 7,
+        name: "E-Tech Gaming Beast",
+        category: "Gaming",
+        price: 169999,
+        image:
+            "https://images.unsplash.com/photo-1593640408182-31c228c5d1b4"
+    },
+
+    {
+        id: 8,
+        name: "E-Tech RGB Keyboard",
+        category: "Gaming",
+        price: 5999,
+        image:
+            "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae"
+    },
+
+    {
+        id: 9,
+        name: "E-Tech Smart Watch",
+        category: "Accessories",
+        price: 14999,
+        image:
+            "https://images.unsplash.com/photo-1546868871-7041f2a55e12"
+    },
+
+    {
+        id: 10,
+        name: "E-Tech Wireless Charger",
+        category: "Accessories",
+        price: 2999,
+        image:
+            "https://images.unsplash.com/photo-1583394838336-acd977736f90"
+    },
+
+    {
+        id: 11,
+        name: "E-Tech Tablet Pro",
+        category: "Accessories",
+        price: 44999,
+        image:
+            "https://images.unsplash.com/photo-1544244015-0df4b3ffc6b0"
+    },
+
+    {
+        id: 12,
+        name: "E-Tech Earbuds X",
+        category: "Audio",
+        price: 7999,
+        image:
+            "https://images.unsplash.com/photo-1606220588913-b3aacb4d2f46"
+    },
+
+    {
+        id: 13,
+        name: "E-Tech Gamer Mouse",
+        category: "Gaming",
+        price: 3499,
+        image:
+            "https://images.unsplash.com/photo-1527814050087-3793815479db"
+    },
+
+    {
+        id: 14,
+        name: "E-Tech Vision Monitor",
+        category: "Gaming",
+        price: 28999,
+        image:
+            "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf"
+    },
+
+    {
+        id: 15,
+        name: "E-Tech Power Bank",
+        category: "Accessories",
+        price: 2499,
+        image:
+            "https://images.unsplash.com/photo-1585336261022-680e295ce3fe"
+    },
+    {
+        id: 16,
+        name: "E-Tech Power Bank",
+        category: "Accessories",
+        price: 2499,
+        image:
+            "https://images.unsplash.com/photo-1585336261022-680e295ce3fe"
+    }
+
+];
